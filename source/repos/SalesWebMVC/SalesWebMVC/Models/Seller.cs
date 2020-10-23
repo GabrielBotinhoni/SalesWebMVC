1e7f5329c3ccfb43ae8bd71bf036d9159a4ed70e
Gabriel Lopes Botinhoni <gabrielbotinhoni@gmail.com>
2020-10-22 21:19:13 -0300

    creatind seed method

--- source/repos/SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ source/repos/SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -19,7 +19,7 @@ namespace SalesWebMVC.Models
 
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
