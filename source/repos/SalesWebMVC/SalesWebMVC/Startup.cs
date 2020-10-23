d6b354adc0c06734b3566812e84d5e5586d9d8db
Gabriel Lopes Botinhoni <gabrielbotinhoni@gmail.com>
2020-10-23 20:35:34 -0300

    Sellers service

--- source/repos/SalesWebMVC/SalesWebMVC/Startup.cs
+++ source/repos/SalesWebMVC/SalesWebMVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -41,6 +42,7 @@ namespace SalesWebMVC
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder =>
                     builder.MigrationsAssembly("SalesWebMVC")));
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
