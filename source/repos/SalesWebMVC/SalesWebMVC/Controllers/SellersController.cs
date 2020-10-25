455c45c0ce263881936fb02fb4383b656f5944e7
Gabriel Lopes Botinhoni <gabrielbotinhoni@gmail.com>
2020-10-25 12:20:34 -0300

    Edit Seller

--- source/repos/SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ source/repos/SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
 using SalesWebMVC.Services;
+using SalesWebMVC.Services.Exceptions;
 using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
@@ -75,6 +76,46 @@ namespace SalesWebMVC.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindByID(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.UpdateSeller(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 
--- source/repos/SalesWebMVC/SalesWebMVC/Services/SellerService.cs
+++ source/repos/SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
@@ -37,6 +38,24 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void UpdateSeller(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("ID not found");
+            }
+            try
+            {
+                
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
     }
